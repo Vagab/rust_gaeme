623678924281a9bc2db6526242f2396a9e9d0397
gorilskij <43991400@users.noreply.github.com>
2019-12-12 17:48:23 +0100

    1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use event_handler::RainMaker;
 
+mod event_handler;
+
+const WIDTH: f32 = 200.;
+const HEIGHT: f32 = 100.;
+
+fn main() {
+    let mut rain_maker = RainMaker::new();
+    rain_maker.run();
 }
