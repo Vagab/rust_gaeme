0e9f2df4c211f47c523fa55f6ef9bf9d99ad1153
Vagab <bilyksergeaa@gmail.com>
2019-12-12 17:51:41 +0100

    rdrop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod r_drop;
+
+const WIDTH: f32 = 200.;
+const HEIGHT; f32 =  100.;
+
 fn main() {
     println!("Hello, world!");
 }
--- src/r_drop.rs
+++ src/r_drop.rs
@@ -1,16 +1,22 @@
 extern crate rand;
 use rand::{thread_rng, prelude::ThreadRng, Rng, distributions::{Normal, Distribution}};
-
+use crate::{WIDTH, HEIGHT};
 
 pub struct RDrop {
-    pub x: f64,
-    pub y: f64,
+    pub x: f32,
+    pub y: f32,
     pub z: u8,
 }
 
 impl RDrop {
 
-    fn gen_pos() {
-        self.x = rand.thread_rng().gen_range(-100.)
+    fn new(r: &mut ThreadRng) -> Self{
+        let r = &mut  rand.thread_rng();
+
+        Self {
+            x: r.gen_range(0., WIDTH),
+            y: r.gen_range(0., HEIGHT),
+            z: r.gen(),
+        }
     }
 }
