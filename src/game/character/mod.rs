4f8f2dff747762857484d985dc95aaf9ad0a0571
gorilskij <43991400@users.noreply.github.com>
2019-12-13 21:36:28 +0100

    1

--- src/game/character/mod.rs
+++ src/game/character/mod.rs
@@ -1,16 +1,24 @@
 pub mod gravity_affected;
 
 use crate::{WIDTH, HEIGHT};
+use crate::game::character::gravity_affected::GravityAffected;
 
 pub struct Character {
     pub x: f32,
     pub y: f32,
 }
 
+impl GravityAffected for Character {
+    fn fall(&mut self, step: f32) {
+        if self.y > HEIGHT || self.y < 0. { return }
+        self.y += step;
+    }
+}
+
 impl Character {
     pub fn new() -> Self {
         Self {
-            x: WIDTH / 2.,
+            x: 100.,
             y: HEIGHT / 2.,
         }
     }
